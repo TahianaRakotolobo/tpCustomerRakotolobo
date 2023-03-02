7fb5cfbf67b13f20d50ae148742903f662d6ff5c
TahianaRakotolobo <tahianarakotolobo@gmail.com>
2023-03-02 22:57:22 +0200

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/itu/tpcustomerrakotolobo/ejb/CustomerManager.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpcustomerrakotolobo.ejb;
+
+import java.util.List;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import mg.itu.tpcustomerrakotolobo.entities.Customer;
+/**
+ *
+ * @author Administrator
+ * 
+ * class dedicated to data management
+ * (select, insert, update)
+ */
+@Stateless
+public class CustomerManager {
+    @PersistenceContext(unitName = "customerPU") 
+    private EntityManager em;
+    
+    /** 
+     * public List<Customer> getAllCustomers()
+     * select all the customers 
+     */
+    public List<Customer> getAllCustomers() {  
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();  
+    }  
+        
+    /** 
+     * public Customer update(Customer customer)
+     * modify a costumer 
+     */
+    public Customer update(Customer customer) {
+        return em.merge(customer);  
+    }
+    
+    /** 
+     * public void persist(Customer customer)
+     * insert a new customer 
+     */
+    public void persist(Customer customer) {   
+        em.persist(customer);
+    }
+}
